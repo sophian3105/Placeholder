015df24fe9ccc75fdd7b25905ed5d1b39071cc05
kimchiloof <kevin.2005.ge@gmail.com>
2023-07-22 15:28:55 -0400

    Comment for clarity

--- app/src/main/java/com/example/placeholder/MainActivity.kt
+++ app/src/main/java/com/example/placeholder/MainActivity.kt
@@ -3,15 +3,15 @@ package com.example.placeholder
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.example.placeholder.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         // Camera Button
         binding.appBarMain.openCamera.setOnClickListener {
-            // Open the camera activity - does not close this activity as of now
+            // Open the camera activity
             val intent = Intent(this, CameraActivity::class.java)
             startActivity(intent)
         }
