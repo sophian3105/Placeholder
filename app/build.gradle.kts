3a435277d1b0fe24baa506eb3935877ee0519f49
kimchiloof <kevin.2005.ge@gmail.com>
2024-09-20 23:23:42 -0400

    Add dependency compose runtime

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.runtime.livedata)
     // Room - data storage
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
