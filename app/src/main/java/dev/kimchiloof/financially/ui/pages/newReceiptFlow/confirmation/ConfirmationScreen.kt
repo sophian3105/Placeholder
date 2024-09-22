eb9c5e6f2dd31e1432525c72a813d0a07c9352f5
kimchiloof <kevin.2005.ge@gmail.com>
2024-09-21 22:34:53 -0400

    Fix ConfirmationScreen UI

--- app/src/main/java/dev/kimchiloof/financially/ui/pages/newReceiptFlow/confirmation/ConfirmationScreen.kt
+++ app/src/main/java/dev/kimchiloof/financially/ui/pages/newReceiptFlow/confirmation/ConfirmationScreen.kt
@@ -1,7 +1,5 @@
 package dev.kimchiloof.financially.ui.pages.newReceiptFlow.confirmation
 
-import android.util.Log
-import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,7 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -32,13 +29,7 @@ fun ConfirmationScreen(navController: NavController, activityViewModel: NewRecei
                 .padding(paddingValues)
                 .verticalScroll(rememberScrollState())
         ) {
-            Text("cofirmation screen")
-            Log.d(
-                "ConfirmationScreen",
-                "Selected image: ${activityViewModel.receiptImage?.absolutePath}"
-            )
             val path = activityViewModel.receiptImage?.path
-            if (path == null) Toast.makeText(context, "whoop", Toast.LENGTH_SHORT).show()
             AsyncImage(
                 model = ImageRequest.Builder(context).data(path).build(),
                 contentDescription = "Captured image",
