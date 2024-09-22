a611aa712b7efb499dd747ac7ec71c8baf118b9c
sophian3105 <sophian3105@gmail.com>
2024-09-22 15:30:18 -0400

    Finding how much is spent per selected day

--- app/src/main/java/dev/kimchiloof/financially/ui/pages/finances/FinancesScreen.kt
+++ app/src/main/java/dev/kimchiloof/financially/ui/pages/finances/FinancesScreen.kt
@@ -3,19 +3,22 @@ package dev.kimchiloof.financially.ui.pages.finances
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.kimchiloof.financially.MainViewModel
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import dev.kimchiloof.financially.utils.DatePickerContent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FinancesScreen() {
+fun FinancesScreen(viewModel: MainViewModel = viewModel()) {
     var selectedDate by remember { mutableStateOf<LocalDate?>(null) }
-    var showDatePicker by remember { mutableStateOf(false) }
+    var showDatePicker by rememberSaveable { mutableStateOf(false) }
     val context = LocalContext.current
 
     Column(modifier = Modifier.padding(16.dp)) {
@@ -40,6 +43,21 @@ fun FinancesScreen() {
                     )
                 }
             }
+        }
+
+        selectedDate?.let { date ->
+            val filteredReceipts by viewModel.getFilteredReceipts(
+                name = null,
+                start = date,
+                end = date,
+                min = null,
+                max = null,
+                category = null
+            ).collectAsState(initial = emptyList())
+
+            val totalAmountSpent = filteredReceipts.sumOf { it?.amount ?: 0.0 }
+
+            Text(text = "Total Amount Spent: $totalAmountSpent")
         }
     }
 }
